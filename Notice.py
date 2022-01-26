74c83686404ca8e9f05f5b72444f66dc3b9674d7
Javier Maties <javier@geekia.es>
2022-01-26 09:57:47 +0100

    Añadido nombre de contacto

--- Notice.py
+++ Notice.py
@@ -5,12 +5,12 @@ from sendgrid.helpers.mail import *
 api_key = config('SENDGRID_API_KEY')
 
 class Send:
-    def mail_15(self, to, type):
+    def mail_15(self, to, type, nombre, precio):
         sg = sendgrid.SendGridAPIClient(api_key)
         from_email = Email("somos@cariz.studio")
         to_email = To(to)
-        subject = "Próximo cargo"
-        content = Content("text/plain", "Dentro de 15 días caducan los servicios que tienes contratados con nosotros")
+        subject = "Renovación de servicios"
+        content = Content("text/html", "<p>Hola "+nombre+"</p><p>Te escribimos porque dentro de quince días caducará el servicio anual de "+type+" que tienes contratado con nosotros con coste total de "+precio+"€+IVA</p><p>Cuando llegue el día, recibirás una factura con el número de cuenta para su abono.<br/>Si decides no seguir con nosotros, por favor contáctanos a la mayor brevedad para que podamos facilitarte la migración de los servicios.<br/><br/>Saludos.</p><p>cariz.studio</p>")
         mail = Mail(from_email, to_email, subject, content)
         response = sg.client.mail.send.post(request_body=mail.get())
         return(response.status_code)
--- main.py
+++ main.py
@@ -32,15 +32,15 @@ class MainWeb:
             aviso = str(aviso_tmp.day)+'-'+str(aviso_tmp.month)
 
             if now == aviso:
-                self.send_notice(customer['email'], dato[2])
+                self.send_notice(customer['email'], dato[0], customer['contact_person'], dato[2])
 
             if now == dato[1]:
                 fecha = str(currentYear)+'-'+str(currentMonth)+'-'+str(currentDay)
                 factura = False
                 if 3 < len(dato): factura = dato[3]
-                self.send_invoice(customer['id'], fecha, customer['email'], dato[0], dato[2], factura)
+                self.send_invoice(customer['id'], fecha, customer['email'], dato[0], dato[2], factura, customer['contact_person'])
 
-    def send_invoice(self, id, fecha, correo, tipo, precio, factura):
+    def send_invoice(self, id, fecha, correo, tipo, precio, factura, name):
         false = False
         true = True
         facturacion = False
@@ -61,8 +61,8 @@ class MainWeb:
         }
 
         bodyCorreo = {
-            'body': 'Aqui llevas tu factura por todo lo que hemos hecho',
-            'subject': 'La factura!!!',
+            'body': 'Hola '+name+'.\n\nAquí te hacemos llegar la factura de renovación anual de '+tipo+'.\n\nMuchas gracias por seguir confiando en nosotros y saludos.\n\ncariz.studio',
+            'subject': 'Factura de servicios',
             'reply_to': 'somos@cariz.studio',
             'to':[correo, 'somos@cariz.studio'],
             'include_pdf': true,
@@ -78,8 +78,8 @@ class MainWeb:
         else:
             print('NADA')
 
-    def send_notice(self, correo, tipo):
-        notice.Send.mail_15(self, correo, tipo)
+    def send_notice(self, correo, tipo, nombre, precio):
+        notice.Send.mail_15(self, correo, tipo, nombre, precio)
  
 
     #API
